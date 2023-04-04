ff3ab65cab73b061b79670e93d7c1f3cc886f2fe
Richard Felintro <richardfelintro06@gmail.com>
2023-04-04 14:39:34 -0300

    Pastas reorganizadas

---
+++ C#/011/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine($"Informe o valor de a:");
+double a = float.Parse(Console.ReadLine());
+Console.WriteLine($"Informe o valor de b:");
+double b = float.Parse(Console.ReadLine());
+Console.WriteLine($"Informe o valor de c:");
+double c = float.Parse(Console.ReadLine());
+Console.WriteLine($"O valor de delta da equação é {(b*b) - 4*a*c}");
