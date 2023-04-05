3cf54f1e4abc8c4a85aea455158acf2e3cecbefb
Richard Felintro <richardfelintro06@gmail.com>
2023-04-05 17:21:55 -0300

    Realizados exercícios 22 a 25

--- C#/024/Program.cs
+++ C#/024/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine($"Informe a distância percorrida em Km");
+double distancia = float.Parse(Console.ReadLine());
+if (distancia > 200){
+    Console.WriteLine($"Sua cobraça é de R${Math.Round(distancia*0.45, 2)}");
+}
+else{
+        Console.WriteLine($"Sua cobraça é de R${Math.Round(distancia*0.5, 2)}");
+}
--- C#/025/Program.cs
+++ C#/025/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine($"Informe o comprimento da primeira reta:");
+double reta1 = double.Parse(Console.ReadLine());
+Console.WriteLine($"Informe o comprimento da segunda reta:");
+double reta2 = double.Parse(Console.ReadLine());
+Console.WriteLine($"Informe o comprimento da terceira reta:");
+double reta3 = double.Parse(Console.ReadLine());
+
+if (reta1 > reta2 + reta3 || reta2 > reta1 + reta3 || reta3 > reta2 + reta1){
+Console.WriteLine($"Este triângulo não é fisicamente possível.");
+}
+else{
+    Console.WriteLine($"Este triângulo é fisicamente possível.");
+}
