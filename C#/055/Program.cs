272fca2b27022d2ef428381b35cb485fde763227
Richard Felintro <richardfelintro06@gmail.com>
2023-04-14 16:33:12 -0300

    Exercícios pulados, exercício desafio 55 realizado.

--- C#/055/Program.cs
+++ C#/055/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 55) [DESAFIO] Vamos melhorar o jogo que fizemos no exercício 32. A partir de
+// agora, o computador vai sortear um número entre 1 e 10 e o jogador vai ter 4
+// tentativas para tentar acertar.
+// 32) [DESAFIO] Crie um jogo onde o computador vai sortear um número entre 1 e 5 o
+// jogador vai tentar descobrir qual foi o valor sorteado.
+Random rnd = new Random();
+int numero = rnd.Next(1, 11);
+Console.WriteLine($"Adivinhe o número!");
+Console.WriteLine($"Insira um número inteiro:");
+int adiv = -5;
+for (var i = 1; i <= 4; i++)
+{
+    adiv = int.Parse(Console.ReadLine());
+    if (adiv == numero)
+    {
+        Console.WriteLine($"PARABÉNS! Você adivinhou que o número correto era {adiv}.");
+        i = 4;
+    }
+    else
+    {
+        if (i == 3)
+        {
+            Console.WriteLine($"ERROU! Ultima tentativa.");
+        }
+        else
+        {
+            Console.WriteLine($"ERROU! Você tem {4 - i} tentativas sobrando.");
+        }
+    }
+}
